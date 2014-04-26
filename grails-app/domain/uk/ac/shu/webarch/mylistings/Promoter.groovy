a067340460a5dd44031bab46bf3ea298d7ceb0a2
student <student@debian>
2014-04-26 22:18:29 +0100

    updated Promoter domain class and generated scaffolds

--- grails-app/domain/uk/ac/shu/webarch/mylistings/Promoter.groovy
+++ grails-app/domain/uk/ac/shu/webarch/mylistings/Promoter.groovy
@@ -2,6 +2,11 @@ package uk.ac.shu.webarch.mylistings
 
 class Promoter {
 
+    String promoterName
+    String promterEmail
+    String promoterTelephone
+    String website
+
     static constraints = {
     }
 }
